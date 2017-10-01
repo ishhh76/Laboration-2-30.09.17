a0f72e52b60c66692cfa514f1cb6c12f61c9e64b
Ishhh <ishrarafi@gmail.com>
2017-10-01 13:09:22 +0200

    Made a new program

---
+++ Laboration-2-30.09.17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_2_30._09._17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
